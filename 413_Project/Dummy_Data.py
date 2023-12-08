38d0d9d184c14734fa8f003a8201cdb1bdc7e6af
Mario Weiler <mariow@arizona.edu>
2023-12-07 18:20:55 -0700

    got the graphs to work

--- 413_Project/Dummy_Data.py
+++ 413_Project/Dummy_Data.py
@@ -7,6 +7,6 @@ for i in range(20):
     myobj = {"event":"hr",
             "HR":"%s"%random.randint(0, 150),
             "SPO2":"%s"%random.randint(0, 150),
-            "coreid":"abc123"}
+            "coreid":"e00fce680993c866c8f5c213"}
     x = requests.post(url, json = myobj)
     time.sleep(1)
