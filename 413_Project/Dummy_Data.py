200e07ba3c63d6ea452780b820bbe0ad72845857
Mario Weiler <mariow@arizona.edu>
2023-12-07 17:17:55 -0700

    updated html pages

--- 413_Project/Dummy_Data.py
+++ 413_Project/Dummy_Data.py
@@ -2,7 +2,7 @@ import requests
 import random
 import time
 
-url = 'http://localhost:3000/api/particle_data'
+url = 'http://localhost:3002/api/particle_data'
 for i in range(20):
     myobj = {"event":"hr",
             "HR":"%s"%random.randint(0, 150),
